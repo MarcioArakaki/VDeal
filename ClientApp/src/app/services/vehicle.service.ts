6ab434f991a5bee3a2b5e1c373394e073dec83f5
Marcio Arakaki <marcio_arakaki@outlook.com>
2019-01-24 20:38:22 -0200

    Refactoring services, creating vehicleService

--- ClientApp/src/app/services/vehicle.service.ts
+++ ClientApp/src/app/services/vehicle.service.ts
@@ -5,11 +5,15 @@ import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
-export class MakeService {
+export class VehicleService {
 
   constructor(private http: HttpClient) { }
 
   getMakes(){
     return this.http.get<any[]>('/api/makes');
+  }
+
+  getFeatures(){
+    return this.http.get<any[]>('/api/features');
   }
 }
